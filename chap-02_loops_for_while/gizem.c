9b8a2271fcdaa551d22be0ea86b0f7563cd7d810
zaferyavuz <yavuzzafer@gmail.com>
2018-07-13 18:38:28 +0300

    Update gizem.c

--- chap-02_loops_for_while/gizem.c
+++ chap-02_loops_for_while/gizem.c
@@ -3,31 +3,21 @@ int main()
 	int i;
 	float a,toplam=0;
 	float ort;
-	int max, min;
+	int max=-999999, min=999999; // başlangıçta max mümkün olduğunca küçük, min ise mümkün olduğunca büyük seçilir.
 	for(i=0; i<10; i++)
 	{
 		printf("10 sayidan %d. sayiyi giriniz: ", i+1);
 		scanf("%f", &a);
 		toplam+=a;
-    // MİN VE MAX BURADA BULUNMASI GEREKİR
+		if(a>max) // max, min bulma kodu
+			max=a;
+		else
+			min=a;
 	}
 	printf("Girilen sayilarin toplami = %f\n", toplam);
 	ort=toplam/10;
 	printf("Girilen sayilarin ortalamasi= %.1f\n", ort);
-	
-	if(i==0)
-	{
-		max=min=a;
-        }
-		for(i=0; i<10; i++)
-	{
-		if(min>a)
-	    	min=a;
-        else if(max<a)
-            max=a;
-	}
 	printf("En kucuk sayi = %d\n", min);
-	printf("En buyuk sayi= %d", max);
-	
+	printf("En buyuk sayi= %d", max);	
 	return 0;
 }
