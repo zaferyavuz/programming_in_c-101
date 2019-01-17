5a12165d0f48640bcd22878d33b197b626399c6e
zaferyavuz <yavuzzafer@gmail.com>
2019-01-17 21:05:54 +0300

    Update 01_file_reading.c

--- 07_file_io/01_file_reading.c
+++ 07_file_io/01_file_reading.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
 main() {
-   FILE *fp;
 
-   fp = fopen("/tmp/test.txt", "w+");
-   fprintf(fp, "This is testing for fprintf...\n");
-   fputs("This is testing for fputs...\n", fp);
+   FILE *fp;
+   char buff[255];
+
+   fp = fopen("/tmp/test.txt", "r");
+   fscanf(fp, "%s", buff);
+   printf("1 : %s\n", buff );
+
+   fgets(buff, 255, (FILE*)fp);
+   printf("2: %s\n", buff );
+   
+   fgets(buff, 255, (FILE*)fp);
+   printf("3: %s\n", buff );
    fclose(fp);
+
 }
